3f859de1cccefc781b6b72b4d3bb0cd689f42a1b
skochauf <stephan.kochauf@lv.campus02.at>
2021-12-20 19:52:09 +0100

    Array => NumberHelper

---
+++ src/org/campus02/rec/DemoMapsApp.java
@@ -0,0 +1,46 @@
+package org.campus02.rec;
+
+public class DemoMapsApp {
+
+    public static void main(String[] args) {
+
+        Ort graz = new Ort("Graz"); // STRG+D
+        Ort wien = new Ort("Wien");
+        Ort marburg = new Ort("Marburg");
+        Ort linz = new Ort("Linz");
+
+        graz.addNachbar(marburg);
+        graz.addNachbar(wien);
+        wien.addNachbar(linz);
+        linz.addNachbar(graz);
+        marburg.addNachbar(graz);
+        wien.addNachbar(graz);
+        linz.addNachbar(wien);
+
+        System.out.println("hasCircle(graz) = " + hasCircle(graz));
+    }
+
+    public static boolean hasCircle(Ort o) {
+        return hasCircle(o, o, null);
+    }
+
+    public static boolean hasCircle(Ort start, Ort current, Ort previous) {
+
+        for (Ort n : current.getNachbarn()) {
+            if (n == previous) {
+                return false;
+            }
+
+            if (n == start) {
+                // Ich bin hier wieder am Startknoten
+                return true;
+            }
+            else {
+                boolean result = hasCircle(start, n, current);
+                if (result)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
